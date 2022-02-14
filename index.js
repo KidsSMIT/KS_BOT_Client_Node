875690d3c8d0ff206e34d98cee59130197a69acc
Codingwithcn <codingwithcn@gmail.com>
2022-02-14 10:00:56 -0500

    fixing versioning issue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 const io = require("socket.io-client");
 
-const {UserDoesNotexist, UnableToSendMessageToServer, EventDoesNotExist} = require("./bot_error");
+const { UserDoesNotexist, UnableToSendMessageToServer, EventDoesNotExist } = require("./bot_error");
 const Events = require("./event");
 
 /**
@@ -11,7 +11,7 @@ class Bot {
     /**
      * The version of the Bot class
      */
-    static __version__ = "2.0.0";
+    static __version__ = "2.5.1";
 
     /**
      * The domain name it will be making request too
@@ -38,7 +38,7 @@ class Bot {
      * @param {str} password - User password
      * @param {boolean} log - Tells bot whether or not it should log its errors to the console
      */
-    constructor(name, password, log=true){
+    constructor(name, password, log = true) {
         this.name = name;
         this.password = password;
         this.log = log
@@ -53,56 +53,56 @@ class Bot {
         this.add_event("starting_login", function() {
             if (log) console.log("Starting to log in")
         })
-          
+
         this.add_event("login_finished", function() {
             if (log) console.log("Finished log in")
         })
-          
+
         this.add_event("start_run", function() {
             if (log) console.log("Run Proccess is starting")
         })
-          
+
         this.add_event("ready_to_run", function() {
             if (log) console.log("KS-BOT is ready to run")
         })
-      
+
         this.add_event("welcome_message", function(data) {
             if (log) console.log("server said hello", data)
         })
-      
+
         this.add_event("bot_reply", function(data) {
             console.log("KS-BOT reply is ready")
             console.log(data)
         })
 
         this.add_event("smith_reply", function(data) {
-          console.log("SMITH is ready to reply")
-          console.log(data)
+            console.log("SMITH is ready to reply")
+            console.log(data)
         })
-      
+
         this.add_event("timer_over", function(data) {
             console.log("KS-BOT said timer is over")
             console.log("Timer: ", data)
         })
 
         this.add_event("activate_smith", function(data) {
-          if (log) console.log("Activating S.M.I.T.H");
+            if (log) console.log("Activating S.M.I.T.H");
         })
 
         this.add_event("de_activate_smith", function(data) {
-          if (log) console.log("DeActivating S.M.I.T.H now");
+            if (log) console.log("DeActivating S.M.I.T.H now");
         })
 
         this.add_event("SPEAK", function(data) {
-          console.log("SMITH want you to say: ", data);
+            console.log("SMITH want you to say: ", data);
         })
 
         this.add_event("News", function(data) {
-          console.log("Your news is: ", data);
+            console.log("Your news is: ", data);
         })
 
         this.add_event("Weather", function(data) {
-          console.log("Your weather is: ", data);
+            console.log("Your weather is: ", data);
         })
     }
 
@@ -117,33 +117,33 @@ class Bot {
         let this1 = this;
 
         fetch(`${Bot.web_url}/does_user_exist`, {
-            method: "POST",
-            body: JSON.stringify({
-                name: name,
-                password: password
-            }),
-            headers: {"Content-type": "application/json"}
-        }).then(res => res.json())
-        .then(json => {
-            if (json.returns === "User exist") {
-                this1.session_id = json.data.id;
-                this1.can_run = true;
-                try {
-                    this1.call_event("login_finished");
-                    this1.start_run();
-                }catch(err) {
-                    if (this1.log) {
-                        this1.errorCallback("Problem in the start run function", err)
+                method: "POST",
+                body: JSON.stringify({
+                    name: name,
+                    password: password
+                }),
+                headers: { "Content-type": "application/json" }
+            }).then(res => res.json())
+            .then(json => {
+                if (json.returns === "User exist") {
+                    this1.session_id = json.data.id;
+                    this1.can_run = true;
+                    try {
+                        this1.call_event("login_finished");
+                        this1.start_run();
+                    } catch (err) {
+                        if (this1.log) {
+                            this1.errorCallback("Problem in the start run function", err)
+                        }
                     }
+                } else {
+                    throw new UserDoesNotexist("User does not exist");
                 }
-            }else {
-                throw new UserDoesNotexist("User does not exist");
-            }
-        }).catch(err => {
-            if (this1.log) {
-                this1.errorCallback("LogIn Failed", err)
-            }
-        })
+            }).catch(err => {
+                if (this1.log) {
+                    this1.errorCallback("LogIn Failed", err)
+                }
+            })
     }
 
     /**
@@ -177,7 +177,7 @@ class Bot {
             this.socket_io.on("SPEAK", this.bot_speak);
             this.socket_io.on("Here is The News", this.news);
             this.socket_io.on("Here is the Weather", this.weather);
-          
+
             this.socket_io.emit("launch_bot", {
                 session_id: this.session_id
             })
@@ -185,45 +185,45 @@ class Bot {
     }
 
     /**
-    * Handles the Here is the Weather event of server
-    * @param {Object.<string, Object>} data - Data sent by server on Here is the Weather event
-    */
+     * Handles the Here is the Weather event of server
+     * @param {Object.<string, Object>} data - Data sent by server on Here is the Weather event
+     */
     weather = (data) => {
-      this.call_event("Weather", [data])
+        this.call_event("Weather", [data])
     }
-  
+
     /**
-    * Handles the Here is The News event of server
-    * @param {Object.<string, Object>} data - Data sent by server on Here is The News event
-    */
+     * Handles the Here is The News event of server
+     * @param {Object.<string, Object>} data - Data sent by server on Here is The News event
+     */
     news = (data) => {
-      this.call_event("News", [data]);
+        this.call_event("News", [data]);
     }
-  
+
     /**
-    * Handles the SPEAK event of server
-    * @param {Object.<string, Object>} data - Data sent by server on SPEAK event
-    */
+     * Handles the SPEAK event of server
+     * @param {Object.<string, Object>} data - Data sent by server on SPEAK event
+     */
     bot_speak = (data) => {
-      this.call_event("SPEAK", [data.what_to_speak]);
+        this.call_event("SPEAK", [data.what_to_speak]);
     }
-  
+
     /**
-    * Handles the Switch To Text Assitant event of server, and then calls the activate_smith event
-    * @param {Object.<string, Object>} data - Data sent by server on Switch To Text Assitant event
-    */
+     * Handles the Switch To Text Assitant event of server, and then calls the activate_smith event
+     * @param {Object.<string, Object>} data - Data sent by server on Switch To Text Assitant event
+     */
     activate_ks_bot = (data) => {
-      this.smit_activated = false;
-      this.call_event("de_activate_smith", [data])
+        this.smit_activated = false;
+        this.call_event("de_activate_smith", [data])
     }
-  
+
     /**
-    * Handles the Switch To Voice Assitant event of server, and then calls the activate_smith event
-    * @param {Object.<string, Object>} data - Data sent by server on Switch To Voice Assitant event
-    */
+     * Handles the Switch To Voice Assitant event of server, and then calls the activate_smith event
+     * @param {Object.<string, Object>} data - Data sent by server on Switch To Voice Assitant event
+     */
     activate_smith = (data) => {
-      this.smit_activated = true;
-      this.call_event("activate_smith", [data])
+        this.smit_activated = true;
+        this.call_event("activate_smith", [data])
     }
 
     /**
@@ -239,11 +239,11 @@ class Bot {
      * @param {Object.<string, Object>} data - Data sent by server in BotProcessReply event
      */
     ProcessBotReply = (data) => {
-      if (this.smit_activated) {
-        this.call_event("smith_reply", [data]);
-      }else {
-        this.call_event("bot_reply", [data]);
-      }
+        if (this.smit_activated) {
+            this.call_event("smith_reply", [data]);
+        } else {
+            this.call_event("bot_reply", [data]);
+        }
     }
 
     /**
@@ -263,7 +263,7 @@ class Bot {
     add_event = (event_name, event_func) => {
         try {
             this.event.add_event(event_name, event_func);
-        }catch(err) {
+        } catch (err) {
             if (this.log) {
                 this.errorCallback(`Problem adding new event "${event_name}"`, err);
             }
@@ -287,10 +287,10 @@ class Bot {
                         timeZone: timeZone || Intl.DateTimeFormat().resolvedOptions().timeZone
                     }
                 })
-            }else {
+            } else {
                 throw new UnableToSendMessageToServer("Problem sending message to server");
             }
-        }catch(err) {
+        } catch (err) {
             if (this.log) this.errorCallback("Problem sending message to server", err)
         }
     }
@@ -300,14 +300,14 @@ class Bot {
      * @param {string} event_name -  Name of the event you would like to call_event
      * @param {Array} event_args  - List argument of function
      */
-    call_event = (event_name, event_args=[]) => {
+    call_event = (event_name, event_args = []) => {
         try {
             if (event_name in this.event.event) {
                 this.event.call_event(event_name, event_args)
-            }else {
+            } else {
                 throw new EventDoesNotExist(`The following event "${event_name}" is not registered to KS-BOT`);
             }
-        }catch(err) {
+        } catch (err) {
             if (this.log) {
                 this.errorCallback(`Problem calling the follwoing event "${event_name}"`, err);
             }
